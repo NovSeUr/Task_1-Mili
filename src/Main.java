c7473515dc4514f6f6b7165b769513681db45186
Nov.s <novikov.su20@gmail.com>
2023-01-31 20:00:49 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Здравствуйте,");
+        int a = 5000;
+        int b = 20;
+        int c = a / b;
+        System.out.println("Мили за билет: " + c);
+    }
+}
